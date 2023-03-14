e3e185d129768d33caa4e0f91ea43b86677f6dc9
Matias <mpintov42@gmail.com>
2023-03-14 19:02:38 -0300

    Initial Commit

---
+++ Storm/StormApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormApp.swift
+//  Storm
+//
+//  Created by Matías Pinto on 14-03-23.
+//
+
+import SwiftUI
+
+@main
+struct StormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
